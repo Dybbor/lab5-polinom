1c691bd29f31fc0e64b87bc2cba2bf01cba12a48
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2019-02-24 15:58:52 +0300

    save

--- Monom/Monom.h
+++ Monom/Monom.h
@@ -34,4 +34,24 @@ bool operator>(const TMonom &m1, const TMonom &m2)
 	return !(m1 > m2);
 }
 
-istream &operator>>(istream &istr, TMonom &m);
+istream &operator>>(istream &istr, TMonom &m) 
+{
+	int coeff, x, y, z;
+	istr >> coeff >> x >> y >> z;
+	m.coeff = coeff;
+	m.PowX = x;
+	m.PowY = y;
+	m.PowZ = z;
+	return istr;
+}
+
+ostream& operator<<(ostream &ostr, const TMonom &m) 
+{
+	if (m.coeff != 0)
+		if (m.coeff != 1);
+
+	ostr << "Empty";
+	if (m.PowX==0)
+
+	return  ostr;
+}
