3c7bd767c1ddad38a98f8d9d1392d4ac0b130296
Vitaly dobrohotov <19dobrohotov99@mail.ru>
2019-03-11 12:41:00 +0300

    some changes

--- lab5/Source.cpp
+++ lab5/Source.cpp
@@ -4,7 +4,31 @@
 int main() 
 {	
 	//Тестовый мейн для полинома
-	setlocale(LC_ALL, "Russian");
+
+	TPolinom p, q,copy;
+	p.InsMonom(InsValueInMonom(-2, 1, 2, 3));
+	p.InsMonom(InsValueInMonom(3, 0, 2, 3));
+	p.InsMonom(InsValueInMonom(5, 0, 0, 1));
+	p.InsMonom(InsValueInMonom(5, 0, 0, 0));
+	q.InsMonom(InsValueInMonom(2, 5, 2, 3));
+	q.InsMonom(InsValueInMonom(4, 0, 2, 3));
+	q.InsMonom(InsValueInMonom(2, 0, 1, 0));
+	q.InsMonom(InsValueInMonom(-5, 0, 0, 0));
+	copy = p;
+	cout << "The first polinom: " << p << endl;
+	cout << "The second polinom: " << q << endl;
+	p += q;
+	cout << "p+q=" << p << endl;
+	cout << endl;
+	p = copy;
+	q.InsMonom(InsValueInMonom(-2, 1, 2, 3));
+	cout << "The first polinom: " << p << endl;
+	cout << "The second polinom: " << q << endl;
+	p -= q;
+	cout << "p-q=" << p << endl;
+
+
+	/*setlocale(LC_ALL, "Russian");
 	TPolinom p, q,copy;
 	p.InsMonom(InsValueInMonom(-2, 1, 2, 3));
 	p.InsMonom(InsValueInMonom(3, 0, 2, 3));
@@ -18,7 +42,7 @@ int main()
 	cout << "The second polinom: " << q << endl;
 	p -= q;
 	cout << "p-q=" << p << endl;
-
+*/
 	/*cout << "The first polinom :" << endl;
 	cin >> p;
 	system("cls");
